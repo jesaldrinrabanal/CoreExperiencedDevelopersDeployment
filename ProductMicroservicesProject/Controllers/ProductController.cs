a4e1a77071c7f33f0fc5833a66c2579b0c0eba27
Jes Aldrin Rabanal <rabanalaldrin@gmail.com>
2023-07-26 16:47:53 +0800

    Add project files.

---
+++ ProductMicroservicesProject/Controllers/ProductController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductMicroservicesProject.Models;
+using ProductMicroservicesProject.Repository;
+using System.Transactions;
+
+namespace ProductMicroservicesProject.Controllers
+{
+    [Route("api/controller")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        // GET:api/Product
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var products = _productRepository.GetProducts();
+            return new OkObjectResult(products);
+        }
+
+        // GET:api/Product/5
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+            var product = _productRepository.GetProductById(id);
+            return new OkObjectResult(product);
+        }
+
+        // POST:api/Product
+        [HttpPost]
+        public IActionResult Post([FromBody] Product product)
+        {
+            using(var scope = new TransactionScope())
+            {
+                _productRepository.InsertProduct(product);
+                scope.Complete();
+                return CreatedAtAction(nameof(Get), new { id = product.Id });
+            }
+        }
+
+        // PUT:api/Product/5
+        [HttpPut]
+        public IActionResult Put([FromBody] Product product)
+        {
+            if(product != null)
+            {
+                using(var scope = new TransactionScope())
+                {
+                    _productRepository.UpdateProduct(product);
+                    scope.Complete();
+                    return new OkResult();
+                }
+            }
+            return new NoContentResult();
+        }
+
+        // DELETE:api/ApiWithAction/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _productRepository.DeleteProduct(id);
+            return new OkResult();
+        }
+    }
+}
---
+++ ProductMicroservicesProject/Repository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using ProductMicroservicesProject.Models;
+
+namespace ProductMicroservicesProject.Repository
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+        Product GetProductById(int productId);
+        void InsertProduct(Product product);
+        void DeleteProduct(int productId);
+        void UpdateProduct(Product product);
+        void Save();
+    }
+}
