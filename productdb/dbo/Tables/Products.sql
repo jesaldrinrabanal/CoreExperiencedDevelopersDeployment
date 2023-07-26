4a982af725ea0dba990c802db0212d99156a8ee8
Jes Aldrin Rabanal <rabanalaldrin@gmail.com>
2023-07-26 16:58:58 +0800

    Added productdb

---
+++ productdb/dbo/Tables/Products.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Products] (
+    [Id]          INT             IDENTITY (1, 1) NOT NULL,
+    [Name]        NVARCHAR (MAX)  NULL,
+    [Description] NVARCHAR (MAX)  NULL,
+    [Price]       DECIMAL (18, 2) NOT NULL,
+    CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
